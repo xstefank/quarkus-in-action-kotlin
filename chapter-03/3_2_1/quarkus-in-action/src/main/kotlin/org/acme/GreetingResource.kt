09871ed037fa60c58690c89151b959aa368aa1f0
xstefank <xstefank122@gmail.com>
2024-12-16 18:31:17 +0100

    3.2.1 Experimenting with application configuration

--- chapter-03/3_2_1/quarkus-in-action/src/main/kotlin/org/acme/GreetingResource.kt
+++ chapter-03/3_2_1/quarkus-in-action/src/main/kotlin/org/acme/GreetingResource.kt
@@ -4,11 +4,12 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import org.eclipse.microprofile.config.inject.ConfigProperty
 
 @Path("/hello")
-class GreetingResource {
+class GreetingResource(@ConfigProperty(name = "greeting") val greeting: String) {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello Quarkus"
+    fun hello() = greeting
 }
