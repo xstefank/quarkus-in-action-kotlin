9795644f694eecd1ebe42d58a6b1802915e2c1f5
xstefank <xstefank122@gmail.com>
2024-12-16 19:34:44 +0100

    3.4.1 Securing the Quarkus application using OpenID Connect and Dev Services

--- chapter-03/3_4_1/quarkus-in-action/src/main/kotlin/org/acme/GreetingResource.kt
+++ chapter-03/3_4_1/quarkus-in-action/src/main/kotlin/org/acme/GreetingResource.kt
@@ -3,8 +3,11 @@ package org.acme
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.SecurityContext
 import org.eclipse.microprofile.config.inject.ConfigProperty
+
 
 @Path("/hello")
 class GreetingResource(@ConfigProperty(name = "greeting") val greeting: String) {
@@ -12,4 +15,12 @@ class GreetingResource(@ConfigProperty(name = "greeting") val greeting: String) 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = greeting
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    fun whoAmI(@Context securityContext: SecurityContext): String {
+        val userPrincipal = securityContext.userPrincipal
+        return userPrincipal?.let { userPrincipal.name } ?: "anonymous"
+    }
 }
