5ffa8afe8faa865ebfeadbfec92fc53f73d0f99c
xstefank <xstefank122@gmail.com>
2024-12-17 17:31:59 +0100

    4.8.1 Working with gRPC and stream

--- chapter-04/4_8_1/inventory-service/src/main/kotlin/org/acme/inventory/grpc/GrpcInventoryService.kt
+++ chapter-04/4_8_1/inventory-service/src/main/kotlin/org/acme/inventory/grpc/GrpcInventoryService.kt
@@ -2,6 +2,7 @@ package org.acme.inventory.grpc
 
 import io.quarkus.grpc.GrpcService
 import io.quarkus.logging.Log
+import io.smallrye.mutiny.Multi
 import io.smallrye.mutiny.Uni
 import jakarta.inject.Inject
 import org.acme.inventory.database.CarInventory
@@ -10,22 +11,25 @@ import org.acme.inventory.model.*
 @GrpcService
 class GrpcInventoryService(@Inject var inventory: CarInventory) : InventoryService {
 
-    override fun add(request: InsertCarRequest): Uni<CarResponse> {
-        val car = Car()
-        car.licensePlateNumber = request.licensePlateNumber
-        car.manufacturer = request.manufacturer
-        car.model = request.model
-        car.id = CarInventory.ids.incrementAndGet()
-        Log.info("Persisting $car")
-        inventory.getCars()!!.add(car)
-        return Uni.createFrom().item(
+    override fun add(requests: Multi<InsertCarRequest>): Multi<CarResponse> {
+        return requests.map { request: InsertCarRequest ->
+            val car = Car()
+            car.licensePlateNumber = request.licensePlateNumber
+            car.manufacturer = request.manufacturer
+            car.model = request.model
+            car.id = CarInventory.ids.incrementAndGet()
+            car
+        }.onItem().invoke { car: Car ->
+            Log.info("Persisting $car")
+            inventory.getCars()!!.add(car)
+        }.map { car: Car ->
             CarResponse.newBuilder()
                 .setLicensePlateNumber(car.licensePlateNumber)
                 .setManufacturer(car.manufacturer)
                 .setModel(car.model)
                 .setId(car.id!!)
                 .build()
-        )
+        }
     }
 
     override fun remove(request: RemoveCarRequest): Uni<CarResponse> {
