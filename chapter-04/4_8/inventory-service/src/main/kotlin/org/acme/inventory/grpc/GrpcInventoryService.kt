2b0090c53f04ddfa72871d3434dd692823968abc
xstefank <xstefank122@gmail.com>
2024-12-17 17:12:02 +0100

    4.8 Implementing a gRPC service using Quarkus

---
+++ chapter-04/4_8/inventory-service/src/main/kotlin/org/acme/inventory/grpc/GrpcInventoryService.kt
@@ -0,0 +1,51 @@
+package org.acme.inventory.grpc
+
+import io.quarkus.grpc.GrpcService
+import io.quarkus.logging.Log
+import io.smallrye.mutiny.Uni
+import jakarta.inject.Inject
+import org.acme.inventory.database.CarInventory
+import org.acme.inventory.model.*
+
+@GrpcService
+class GrpcInventoryService(@Inject var inventory: CarInventory) : InventoryService {
+
+    override fun add(request: InsertCarRequest): Uni<CarResponse> {
+        val car = Car()
+        car.licensePlateNumber = request.licensePlateNumber
+        car.manufacturer = request.manufacturer
+        car.model = request.model
+        car.id = CarInventory.ids.incrementAndGet()
+        Log.info("Persisting $car")
+        inventory.getCars()!!.add(car)
+        return Uni.createFrom().item(
+            CarResponse.newBuilder()
+                .setLicensePlateNumber(car.licensePlateNumber)
+                .setManufacturer(car.manufacturer)
+                .setModel(car.model)
+                .setId(car.id!!)
+                .build()
+        )
+    }
+
+    override fun remove(request: RemoveCarRequest): Uni<CarResponse> {
+        val optionalCar = inventory.getCars()!!.stream()
+            .filter { car: Car ->
+                (request.licensePlateNumber
+                        == car.licensePlateNumber)
+            }
+            .findFirst()
+        if (optionalCar.isPresent) {
+            val removedCar = optionalCar.get()
+            inventory.getCars()!!.remove(removedCar)
+            return Uni.createFrom().item(
+                CarResponse.newBuilder()
+                    .setLicensePlateNumber(removedCar.licensePlateNumber)
+                    .setManufacturer(removedCar.manufacturer)
+                    .setModel(removedCar.model)
+                    .setId(removedCar.id!!).build()
+            )
+        }
+        return Uni.createFrom().nullItem()
+    }
+}
