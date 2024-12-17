caf21569b0e33cea80757760d565e65ca80ccfb3
xstefank <xstefank122@gmail.com>
2024-12-17 16:03:16 +0100

    Start - 4.5.4 Consuming the Inventory service using a GraphQL client

---
+++ chapter-04/4_5_4/reservation-service/src/main/kotlin/org/acme/reservation/rest/ReservationResource.kt
@@ -0,0 +1,56 @@
+package org.acme.reservation.rest
+
+import io.quarkus.logging.Log
+import jakarta.ws.rs.*
+import jakarta.ws.rs.core.MediaType
+import org.acme.reservation.inventory.Car
+import org.acme.reservation.inventory.InventoryClient
+import org.acme.reservation.rental.RentalClient
+import org.acme.reservation.reservation.Reservation
+import org.acme.reservation.reservation.ReservationsRepository
+import org.eclipse.microprofile.rest.client.inject.RestClient
+import org.jboss.resteasy.reactive.RestQuery
+import java.time.LocalDate
+
+@Path("reservation")
+@Produces(MediaType.APPLICATION_JSON)
+class ReservationResource(
+    private val reservationsRepository: ReservationsRepository,
+    private val inventoryClient: InventoryClient,
+    @RestClient private val rentalClient: RentalClient
+) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    fun make(reservation: Reservation): Reservation {
+        val result = reservationsRepository.save(reservation)
+        // this is just a dummy value for the time being
+        val userId = "x"
+        if (reservation.startDay == LocalDate.now()) {
+            val rental = rentalClient.start(userId, result.id)
+            Log.info("Successfully started rental $rental")
+        }
+        return result
+    }
+
+    @GET
+    @Path("availability")
+    fun availability(@RestQuery startDate: LocalDate?, @RestQuery endDate: LocalDate?): Collection<Car> {
+        // obtain all cars from inventory
+        val availableCars = inventoryClient.allCars()
+        // create a map from id to car
+        val carsById: MutableMap<Long?, Car> = HashMap()
+        for (car in availableCars!!) {
+            carsById[car!!.id] = car
+        }
+
+        // get all current reservations
+        val reservations = reservationsRepository.findAll()
+        // for each reservation, remove the car from the map
+        for (reservation in reservations) {
+            if (reservation.isReserved(startDate, endDate)) {
+                carsById.remove(reservation.carId)
+            }
+        }
+        return carsById.values
+    }
+}
