30b23c00e9082b378f7e5ef771ef3d9ff04c69c8
xstefank <xstefank122@gmail.com>
2024-12-17 21:32:49 +0100

    Start - 6.1.2 Adding security to the application

---
+++ chapter-06/6_1_2/reservation-service/src/test/kotlin/org/acme/reservation/ReservationRepositoryTest.kt
@@ -0,0 +1,27 @@
+package org.acme.reservation
+
+import io.quarkus.test.junit.QuarkusTest
+import jakarta.inject.Inject
+import org.acme.reservation.reservation.Reservation
+import org.acme.reservation.reservation.ReservationsRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+
+@QuarkusTest
+class ReservationRepositoryTest {
+    @Inject
+    lateinit var repository: ReservationsRepository
+
+    @Test
+    fun testCreateReservation() {
+        val reservation = Reservation()
+        reservation.startDay = LocalDate.now().plus(5, ChronoUnit.DAYS)
+        reservation.endDay = LocalDate.now().plus(12, ChronoUnit.DAYS)
+        reservation.carId = 384L
+        repository.save(reservation)
+        Assertions.assertNotNull(reservation.id)
+        Assertions.assertTrue(repository.findAll().contains(reservation))
+    }
+}
