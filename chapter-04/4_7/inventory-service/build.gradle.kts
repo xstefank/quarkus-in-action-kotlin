7a0c3e75b62496ce368661ffab105dbe54e20bdb
xstefank <xstefank122@gmail.com>
2024-12-17 16:52:35 +0100

    4.7 Adding gRPC support to your project

--- chapter-04/4_7/inventory-service/build.gradle.kts
+++ chapter-04/4_7/inventory-service/build.gradle.kts
@@ -14,6 +14,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-grpc")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
     implementation("io.quarkus:quarkus-smallrye-graphql")
