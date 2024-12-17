a980f7ad57a09b8fa0eb18696b427b04f9998389
xstefank <xstefank122@gmail.com>
2024-12-17 13:04:54 +0100

    4.2.2 Making a reservation

--- chapter-04/4_2_2/reservation-service/src/main/kotlin/org/acme/reservation/rest/ReservationResource.kt
+++ chapter-04/4_2_2/reservation-service/src/main/kotlin/org/acme/reservation/rest/ReservationResource.kt
@@ -1,11 +1,10 @@
 package org.acme.reservation.rest
 
-import jakarta.ws.rs.GET
-import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
+import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
 import org.acme.reservation.inventory.Car
 import org.acme.reservation.inventory.InventoryClient
+import org.acme.reservation.reservation.Reservation
 import org.acme.reservation.reservation.ReservationsRepository
 import org.jboss.resteasy.reactive.RestQuery
 import java.time.LocalDate
@@ -13,6 +12,13 @@ import java.time.LocalDate
 @Path("reservation")
 @Produces(MediaType.APPLICATION_JSON)
 class ReservationResource(private val reservationsRepository: ReservationsRepository, private val inventoryClient: InventoryClient) {
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    fun make(reservation: Reservation?): Reservation {
+        return reservationsRepository.save(reservation!!)
+    }
+
     @GET
     @Path("availability")
     fun availability(@RestQuery startDate: LocalDate?, @RestQuery endDate: LocalDate?): Collection<Car> {
