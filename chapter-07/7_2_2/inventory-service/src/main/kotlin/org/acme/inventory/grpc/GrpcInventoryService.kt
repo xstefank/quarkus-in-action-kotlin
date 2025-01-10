7ab07f11429d7a614ec03e93adba7cda46e822bd
xstefank <xstefank122@gmail.com>
2025-01-10 18:10:53 +0100

    7.2.2 Using the repository pattern in Inventory

--- chapter-07/7_2_2/inventory-service/src/main/kotlin/org/acme/inventory/grpc/GrpcInventoryService.kt
+++ chapter-07/7_2_2/inventory-service/src/main/kotlin/org/acme/inventory/grpc/GrpcInventoryService.kt
@@ -2,54 +2,52 @@ package org.acme.inventory.grpc
 
 import io.quarkus.grpc.GrpcService
 import io.quarkus.logging.Log
+import io.quarkus.narayana.jta.QuarkusTransaction
+import io.smallrye.common.annotation.Blocking
 import io.smallrye.mutiny.Multi
 import io.smallrye.mutiny.Uni
 import jakarta.inject.Inject
-import org.acme.inventory.database.CarInventory
+import jakarta.transaction.Transactional
 import org.acme.inventory.model.*
+import org.acme.inventory.repository.CarRepository
 
 @GrpcService
-class GrpcInventoryService(@Inject var inventory: CarInventory) : InventoryService {
+class GrpcInventoryService(@Inject var carRepository: CarRepository) : InventoryService {
 
+    @Blocking
     override fun add(requests: Multi<InsertCarRequest>): Multi<CarResponse> {
         return requests.map { request: InsertCarRequest ->
             val car = Car()
             car.licensePlateNumber = request.licensePlateNumber
             car.manufacturer = request.manufacturer
             car.model = request.model
-            car.id = CarInventory.ids.incrementAndGet()
             car
         }.onItem().invoke { car: Car ->
-            Log.info("Persisting $car")
-            inventory.getCars()!!.add(car)
-        }.map { car: Car ->
-            CarResponse.newBuilder()
-                .setLicensePlateNumber(car.licensePlateNumber)
-                .setManufacturer(car.manufacturer)
-                .setModel(car.model)
-                .setId(car.id!!)
-                .build()
+            QuarkusTransaction.requiringNew().run {
+                carRepository.persist(car)
+                Log.info("Persisting $car")
+            }
+        }.map { car: Car -> CarResponse.newBuilder()
+            .setLicensePlateNumber(car.licensePlateNumber)
+            .setManufacturer(car.manufacturer)
+            .setModel(car.model)
+            .setId(car.id!!)
+            .build()
         }
     }
 
+    @Blocking
+    @Transactional
     override fun remove(request: RemoveCarRequest): Uni<CarResponse> {
-        val optionalCar = inventory.getCars()!!.stream()
-            .filter { car: Car ->
-                (request.licensePlateNumber
-                        == car.licensePlateNumber)
-            }
-            .findFirst()
-        if (optionalCar.isPresent) {
-            val removedCar = optionalCar.get()
-            inventory.getCars()!!.remove(removedCar)
-            return Uni.createFrom().item(
-                CarResponse.newBuilder()
-                    .setLicensePlateNumber(removedCar.licensePlateNumber)
-                    .setManufacturer(removedCar.manufacturer)
-                    .setModel(removedCar.model)
-                    .setId(removedCar.id!!).build()
-            )
-        }
-        return Uni.createFrom().nullItem()
+        val optionalCar = carRepository.findByLicensePlateNumberOptional(request.licensePlateNumber)
+        return optionalCar?.let {
+            carRepository.delete(optionalCar)
+            Uni.createFrom().item(CarResponse.newBuilder()
+                .setLicensePlateNumber(optionalCar.licensePlateNumber)
+                .setManufacturer(optionalCar.manufacturer)
+                .setModel(optionalCar.model)
+                .setId(optionalCar.id!!)
+                .build())
+        } ?: Uni.createFrom().nullItem()
     }
 }
--- chapter-07/7_2_2/inventory-service/src/main/kotlin/org/acme/inventory/service/GraphQLInventoryService.kt
+++ chapter-07/7_2_2/inventory-service/src/main/kotlin/org/acme/inventory/service/GraphQLInventoryService.kt
@@ -1,36 +1,37 @@
 package org.acme.inventory.service
 
+import io.quarkus.logging.Log
 import jakarta.inject.Inject
-import org.acme.inventory.database.CarInventory
+import jakarta.transaction.Transactional
 import org.acme.inventory.model.Car
+import org.acme.inventory.repository.CarRepository
 import org.eclipse.microprofile.graphql.GraphQLApi
 import org.eclipse.microprofile.graphql.Mutation
 import org.eclipse.microprofile.graphql.Query
 
 @GraphQLApi
-class GraphQLInventoryService(@Inject var inventory: CarInventory) {
+class GraphQLInventoryService(@Inject var carRepository: CarRepository) {
 
     @Query
-    fun cars(): List<Car>? {
-        return inventory.getCars()
+    fun cars(): List<Car> {
+        return carRepository.listAll()
     }
 
+    @Transactional
     @Mutation
     fun register(car: Car): Car {
-        car.id = CarInventory.ids.incrementAndGet()
-        inventory.getCars()!!.add(car)
+        carRepository.persist(car)
+        Log.info("Persisting $car")
         return car
     }
 
+    @Transactional
     @Mutation
     fun remove(licensePlateNumber: String): Boolean {
-        val cars = inventory.getCars()
-        val toBeRemoved = cars!!.stream()
-            .filter { car: Car -> car.licensePlateNumber == licensePlateNumber }.findAny()
-        return if (toBeRemoved.isPresent) {
-            cars.remove(toBeRemoved.get())
-        } else {
-            false
-        }
+        val toBeRemoved = carRepository.findByLicensePlateNumberOptional(licensePlateNumber)
+        return toBeRemoved?.let {
+            carRepository.delete(toBeRemoved)
+            true
+        } ?: false
     }
 }
